fc54b8c35e191e97dc2eba3548836803cace8ace
siddhantgupta547 <sidgpt84@gmail.com>
2022-01-15 19:43:03 +0530

    Add homepage component

---
+++ clothing-website/src/HomePage/HomePage.jsx
@@ -0,0 +1,42 @@
+import './HomePage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HAT</div>
+            <span classname="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HAT</div>
+            <span classname="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HAT</div>
+            <span classname="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HAT</div>
+            <span classname="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HAT</div>
+            <span classname="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;

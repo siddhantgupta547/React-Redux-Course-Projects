60297855c6a34c0d09ea439a76c56910804013f2
siddhantgupta547 <sidgpt84@gmail.com>
2022-01-18 20:39:01 +0530

    Add Navbar

--- clothing-website/src/App.js
+++ clothing-website/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Navbar from './Navbar/Navbar';
 import HomePage from './HomePage/HomePage';
 import ShopPage from './ShopPage/ShopPage';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
---
+++ clothing-website/src/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Navbar.styles.scss';
+import logo from '../crown.svg';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="options">
+        <Link to="/shop" className="option">
+          Shop
+        </Link>
+        <Link to="/contact" className="option">
+          Contact Us
+        </Link>
+        <Link to="/signin" className="option">
+          Sign In
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
